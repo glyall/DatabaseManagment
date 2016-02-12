9d5a4ae20000eae45e8d054d4c3564147d78d98e
glyall <gregory.lyall1@marist.edu>
2016-02-11 19:27:29 -0500

    Lab for is born
    Creation of lab for file and doc

---
+++ Lab 4/Lab questions.sql
@@ -0,0 +1,6 @@
+﻿--1 get cities of agents booking an order from a customers whose cid is 'c002'.
+Select city 
+from agents 
+where aid in (select aid
+	      from orders 
+	      where cid = 'c002');

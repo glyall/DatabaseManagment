9e668449e20c3ac76c40b0e2117e89fc33ed4a12
glyall <gregory.lyall1@marist.edu>
2016-04-28 18:40:44 -0400

    Answered question 2
    Hey sorry this is late, i forgot to update the sql file in github

---
+++ Lab 10/Functions.sql
@@ -0,0 +1,38 @@
+﻿-- Gregory Lyall
+--Question 1 of Lab 10 
+create or replace function preRegsFor(int, REFCURSOR) returns refcursor as 
+$$
+declare
+   preRegNum int       := $1;
+   resultset REFCURSOR := $2;
+begin
+   open resultset for 
+      select *
+      from  Prerequisites
+     order by courseNum desc;
+   return resultset;
+end;
+$$ 
+language plpgsql;
+
+select preRegsFor(0, 'results');
+Fetch all from results;
+-- Question 2 of Lab 10
+create or replace function IsPreReqsFor(int, REFCURSOR) returns refcursor as
+$$
+declare
+	numberIn int       := $1;
+	resultset   REFCURSOR := $2;
+begin
+	open resultset for
+		select preReqNum, courseNum
+		from Prerequisites, Courses
+		where courseNum = num
+		and numberIn = PreReqNum;
+	return resultset;
+end;
+$$
+language plpgsql;
+
+select IsPreReqsFor(220, 'results');
+Fetch all from results;
